dee07e71a0ed7edf5579641dfd2cf93b78f0444a
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-21 13:28:57 -0500

    change column for_bill? to user_opinion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_30_204229) do
+ActiveRecord::Schema.define(version: 2019_11_21_173011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_08_30_204229) do
     t.string "description"
     t.string "bill_url"
     t.string "legislative_day"
-    t.boolean "for_bill?"
+    t.boolean "user_opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
