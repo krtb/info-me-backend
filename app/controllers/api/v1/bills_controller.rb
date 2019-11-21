502f9a8c957129d660ea0a9f19fff418c3042385
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-20 19:50:36 -0500

    organize methods for bills_controller

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -1,49 +1,54 @@
 class Api::V1::BillsController < ApplicationController
- before_action :find_bill, only: [:show, :destroy, :update]
-#  skip_before_action :authorized, only: [:index]
+  before_action :find_bill, only: [:show, :destroy, :update]
+  # skip_before_action :authorized, only: [:index]
 
- def index
-   @bills = Bill.all
-   render json: { bills: Bill.all}
- end
+  # GET /api/v1/bills
+  def index
+    @bills = Bill.all
+    render json: { bills: Bill.all}
+  end
 
- def new
-   @bill = Bill.create
- end
+  # POST /api/v1/bills
+  def create
+    @bill = Bill.create(bill_params)
+    render json: {bill: @bill}
+  end
 
- def show
-   # @bill = Bill.find(params[:id])
-   render json: { bill: @bill}
- end
+  # GET /api/v1/bills/:id
+  def show
+    render json: { bill: @bill}
+  end
 
- def create
-   @bill = Bill.create(bill_params)
-   render json: {bill: @bill}
- end
+  # PUT /api/v1/bills/:id
+  def update
+    # @bill = Bill.find(params[:id])
+    @bill.update(bill_params)
+    if @bill.save
+      render json: @bill, status: :accepted
+    else
+      render json: { errors: @bill.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
 
- def update
-   # @bill = Bill.find(params[:id])
-   @bill.update(bill_params)
-   if @bill.save
-     render json: @bill, status: :accepted
-   else
-     render json: { errors: @bill.errors.full_messages }, status: :unprocessible_entity
-   end
- end
+  # DELETE /api/v1/bills/:id
+  def destroy
+    @bill.destroy
+    render json: @bill, status: :accepted
+  end
 
- def destroy
-   # @post = post.find(params[:id])
-   @bill.destroy
-   render json: @bill, status: :accepted
- end
+  def new
+    @bill = Bill.create
+  end
 
  private
 
- def bill_params
-   params.permit(:api_bill_id, :for_bill?)
- end
+  # WHITELIST params
+  def bill_params
+    params.permit(:api_bill_id, :for_bill?)
+  end
 
- def find_bill
-   @bill = Bill.find(params[:id])
- end
+  def find_bill
+    @bill = Bill.find(params[:id])
+  end
+
 end
