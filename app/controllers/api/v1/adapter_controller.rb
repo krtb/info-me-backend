914c4511812f741b6cc61357a886e92feb609c41
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-21 17:03:57 -0500

    properly indent adapter controller code

--- app/controllers/api/v1/adapter_controller.rb
+++ app/controllers/api/v1/adapter_controller.rb
@@ -1,30 +1,31 @@
 class Api::V1::AdapterController < ApplicationController
-    skip_before_action :authorized, only: %i[fetch_bills]
+  skip_before_action :authorized, only: %i[fetch_bills]
 
+  def fetch_bills
+    bill_api_url = 'https://api.propublica.org/congress/v1/bills/upcoming/house.json'
 
-    def fetch_bills
-      bill_api_url = 'https://api.propublica.org/congress/v1/bills/upcoming/house.json'
+    header =  {
+      "X-API-Key": "zSbuRGW96UaVnIyWENZoHMaFq5lsTJgctPo7952M",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*" 
+      # above line fixed CORS cross orignin request issue
+    }
+      
+    bill_get_response =  RestClient.get(bill_api_url, header)
+    parsed_bill = JSON.parse(bill_get_response.body)
 
-        header =  {
-            "X-API-Key": "zSbuRGW96UaVnIyWENZoHMaFq5lsTJgctPo7952M",
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*" # this line fixed CORS cross orignin request issue
-        }
-        bill_get_response =  RestClient.get(bill_api_url, header)
-
-        parsed_bill = JSON.parse(bill_get_response.body)
-        parsed_bill["results"][0]["bills"].each do |bill|
-
-            Bill.find_or_create_by( chamber: bill["chamber"],
-                                    bill_number:bill["bill_number"], 
-                                    legislative_day: bill["legislative_day"], 
-                                    description: bill["description"],
-                                    bill_url: bill["bill_url"]
-                                  )
-                                  
-        end
-        render json: parsed_bill
+    parsed_bill["results"][0]["bills"].each do |bill|
+      Bill.find_or_create_by(
+        chamber: bill["chamber"],
+        bill_number:bill["bill_number"], 
+        legislative_day: bill["legislative_day"], 
+        description: bill["description"],
+        bill_url: bill["bill_url"]
+      )                       
     end
 
+    render json: parsed_bill
+
+  end
     
 end
