eb4bea1eb3b290608de38be9189146cd380e0551
Kurt Bauer <kurtbauer55@protonmail.com>
2019-11-20 19:43:17 -0500

    organize methods for user_bills_controller

--- app/controllers/api/v1/user_bills_controller.rb
+++ app/controllers/api/v1/user_bills_controller.rb
@@ -1,26 +1,25 @@
 class Api::V1::UserBillsController < ApplicationController
  before_action :find_user_bill, only: [:show, :destroy, :update]
+
+ # GET /api/v1/user_bills
  def index
    @user_bills = UserBill.all
    render json: { user_bills: UserBill.all}
  end
 
- def new
-   @user_bill = UserBill.create
- end
-
- def show
-   # @user_bill = UserBill.find(params[:id])
-   render json: { user_bill: @user_bill}
- end
-
- def create
+  # POST /api/v1/user_bills
+  def create
    @user_bill = UserBill.create(user_bill_params)
    render json: {user_bill: @user_bill}
- end
+  end
 
- def update
-   # @user_bill = UserBill.find(params[:id])
+  # GET  /api/v1/user_bills/:id
+  def show
+   render json: { user_bill: @user_bill}
+  end
+
+  # POST /api/v1/user_bills/:id
+  def update
    @user_bill.update(user_bill_params)
    if @user_bill.save
      render json: @user_bill, status: :accepted
@@ -29,19 +28,24 @@ class Api::V1::UserBillsController < ApplicationController
    end
  end
 
- def destroy
-   # @post = post.find(params[:id])
-   @user_bill.destroy
-   render json: @user_bill, status: :accepted
- end
+  # DELETE /api/v1/user_bills/:id
+  def destroy
+    @user_bill.destroy
+    render json: @user_bill, status: :accepted
+  end
+
+  def new
+    @user_bill = UserBill.create
+  end
 
  private
+ # WHITELIST params
+  def user_bill_params
+    params.permit(:user_id, :bill_id)
+  end
 
- def user_bill_params
-   params.permit(:user_id, :bill_id)
- end
+  def find_user_bill
+    @user_bill = UserBill.find(params[:id])
+  end
 
- def find_user_bill
-   @user_bill = UserBill.find(params[:id])
- end
 end
