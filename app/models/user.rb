023916dae16cc8862074bae3e577ba0218e93965
Kurt Bauer <kurtbauer55@protonmail.com>
2020-02-11 13:12:05 -0500

    add has-many-through associations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_bills
-  has_many :bills, through: :user_bills
+  has_many :bills, :through => :user_bills
+  has_many :user_picked_bills
+  has_many :picked_bills, :through => :user_picked_bills
   has_secure_password
   validates :name, uniqueness: { case_sensitive: false }
 end
