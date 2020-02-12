a0eee1f41ccf95ee26b4155062c6d772d8e9d86b
Kurt Bauer <kurtbauer55@protonmail.com>
2020-02-12 11:00:22 -0500

    add validations that span across multiple columns

--- app/models/picked_bill.rb
+++ app/models/picked_bill.rb
@@ -1,4 +1,6 @@
 class PickedBill < ApplicationRecord
-  has_many :user_picked_bills
-  has_many :users, through: :user_picked_bills
+  # validation spans across multiple columns, for has-many-through duplicate issue
+  validates_uniqueness_of :bill_number, :scope => :user_id
+  has_many :user_picked_bills, dependent:  :destroy
+  has_many :users, :through => :user_picked_bills
 end
